e42b1591c07aa7b131dd2a9062fb1e14477dff5a
Lura Skye <l@veriny.tf>
2023-08-09 16:58:23 +0100

    add extended validation for abilities

--- src/reborn_rebalance/scripts/validate.py
+++ src/reborn_rebalance/scripts/validate.py
@@ -48,12 +48,45 @@ def extended_validate_megas(catalog: EssentialsCatalog):
                 )
 
 
+def extended_validate_abilities(catalog: EssentialsCatalog):
+    for species in catalog.species:
+        if len(species.full_abilities) > 3:
+            print(
+                f"warning: species {species.name} has too many abilities ({species.full_abilities})"
+            )
+
+        if len(set(species.full_abilities)) != len(species.full_abilities):
+            print(
+                f"warning: species {species.name} has duplicate abilities"
+                f" ({species.full_abilities})"
+            )
+
+        if species.internal_name not in catalog.forms:
+            continue
+
+        for form in catalog.forms[species.internal_name].forms.values():
+            attrs = form.combined_attributes(species)
+            if len(attrs.raw_abilities) > 3:
+                print(
+                    f"warning: species {species.name} form {form.form_name} has too many abilities"
+                    f" ({attrs.raw_abilities})"
+                )
+
+            if len(set(attrs.raw_abilities)) != len(attrs.raw_abilities):
+                print(
+                    f"warning: species {species.name} form {form.form_name} has duplicate abilities"
+                    f" ({attrs.raw_abilities})"
+                )
+
+
 def do_extended_validation():
     with contextlib.redirect_stdout(StringIO()):
         catalog = EssentialsCatalog.load_from_toml(Path(sys.argv[1]))
 
     print("=== Begin Extended Validation ===\n")
     extended_validate_megas(catalog)
+    print()
+    extended_validate_abilities(catalog)
 
 
 if __name__ == "__main__":
