a49f3a42b917a0f2193a8d70fffed298373f12be
Lura Skye <l@veriny.tf>
2023-08-10 11:10:07 +0100

    allow brave users through

--- templates/static/dechromify.js
+++ templates/static/dechromify.js
@@ -1,4 +1,7 @@
-if (window.chrome) {
+const isBrave = (navigator.brave || false)
+const isChrome = !isBrave && (window.chrome || false);
+
+if (isChrome) {
     document.getElementById("content").remove();
     document.getElementById("antichrome").classList.add("is-active");
 }
